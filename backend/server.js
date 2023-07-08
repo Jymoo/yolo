b79ec7142d5899ca76a05bb70a4dc184e7f26276
Jymo <lugayelivingstone@gmail.com>
2023-07-08 11:08:19 +0300

    implemented mongodb connection url in the serves.js to link mongo to my backend successfully

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
+let mongodb_url = 'mongodb+srv://mongo:EGR8eRYJBXoqcppX@cluster0.kj5eajm.mongodb.net/?retryWrites=true&w=majority';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
